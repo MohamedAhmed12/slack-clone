62082faac0be112f3217e8133c0da27f0334d624
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-07-16 18:06:09 +0300

    feat(channel): create channel mutation

--- models/channel.model.js
+++ models/channel.model.js
@@ -3,6 +3,9 @@ import { DataTypes, models } from "sequelize";
 module.exports = (sequelize) => {
     const Channel = sequelize.define("Channel", {
         name: DataTypes.STRING,
-        public: DataTypes.BOOLEAN,
+        public: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
     });
 };
