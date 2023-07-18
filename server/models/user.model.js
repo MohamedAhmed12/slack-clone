9eb87d7b6519aa5d89cc58a423f46326dab4724c
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-07-18 21:56:47 +0300

    feat(auth): register user validations

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,10 +11,26 @@ module.exports = (sequelize) => {
         username: {
             type: DataTypes.STRING,
             unique: true,
+            validate: {
+                isAlphanumeric: {
+                    args: true,
+                    msg: "Username can only contain letters and numbers",
+                },
+                len: {
+                    args: [3, 25],
+                    msg: "Username need to be between 3 and 25 characters long",
+                },
+            },
         },
         email: {
             type: DataTypes.STRING,
             unique: true,
+            validate: {
+                isEmail: {
+                    args: true,
+                    msg: "Invalid email",
+                },
+            },
         },
         password: {
             type: DataTypes.STRING,
--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,5 +1,7 @@
 import { hash } from "bcrypt";
 
+import { formatErr } from "../helpers/formatError";
+
 export default {
     Query: {
         getUser: (parent, { id }, { models: { User } }) => User.findOne({ where: { id } }),
@@ -8,11 +10,30 @@ export default {
     Mutation: {
         register: async (parent, { password, ...otherArgs }, { models: { User } }) => {
             try {
+                if (password.length < 5 || password.length > 100) {
+                    return {
+                        ok: false,
+                        errors: [
+                            {
+                                path: "password",
+                                message: "Password need to be between 8 and 100 characters long",
+                            },
+                        ],
+                    };
+                }
+
                 const hashedPassword = await hash(password, 12);
-                await User.create({ ...otherArgs, password: hashedPassword });
-                return true;
-            } catch (error) {
-                return false;
+                const user = await User.create({ ...otherArgs, password: hashedPassword });
+
+                return {
+                    ok: true,
+                    user,
+                };
+            } catch (err) {
+                return {
+                    ok: false,
+                    errors: formatErr(err),
+                };
             }
         },
     },
