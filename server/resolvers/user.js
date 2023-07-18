eb1cbcf8a93da31fcc909db86b99486d0200ce32
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-07-18 15:59:48 +0300

    feat(user): hash password at registration

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,9 +1,19 @@
+import { hash } from "bcrypt";
+
 export default {
     Query: {
         getUser: (parent, { id }, { models: { User } }) => User.findOne({ where: { id } }),
         allUsers: (parent, args, { models: { User } }) => User.findAll(),
     },
     Mutation: {
-        register: async (parent, args, { models: { User } }) => await User.create(args),
+        register: async (parent, { password, ...otherArgs }, { models: { User } }) => {
+            try {
+                const hashedPassword = await hash(password, 12);
+                await User.create({ ...otherArgs, password: hashedPassword });
+                return true;
+            } catch (error) {
+                return false;
+            }
+        },
     },
 };
