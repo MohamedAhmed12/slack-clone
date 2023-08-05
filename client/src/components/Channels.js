5587a8322916515fcb2c989e9ab017cf7ac4459a
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-08-05 05:24:23 +0300

    fix(channel): show invite people & create channel only to team owner

--- client/src/components/Channels.js
+++ client/src/components/Channels.js
@@ -1,33 +1,46 @@
-/* eslint-disable */
 import React from "react";
-import { Grid, Icon, Menu } from "semantic-ui-react";
+import { Grid, Menu } from "semantic-ui-react";
 
 import "../assets/components/channels.scss";
 import { Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-const Channels = ({ team, username, channels, users, onAddChannelClick, onInvitePeopleClick }) => {
+const Channels = ({
+    team,
+    currentUser,
+    channels,
+    users,
+    onAddChannelClick,
+    onInvitePeopleClick,
+}) => {
     const Bubble = ({ on = true }) => (on ? <span className="green">●</span> : "○");
+    const isOwner = currentUser.id == team.owner_id;
 
     return (
         <Grid.Column className="channels" width={11}>
             <Menu.Item className="header">
                 {team.name}
-                {username}
+                {currentUser.username}
             </Menu.Item>
             <Menu.Item>
                 <h3>
                     Channels
-                    <Button
-                        onClick={onAddChannelClick}
-                        icon="add circle"
-                        className="transparent-button"
-                    />
+                    {isOwner && (
+                        <Button
+                            onClick={onAddChannelClick}
+                            icon="add circle"
+                            className="transparent-button"
+                        />
+                    )}
                 </h3>
 
                 {channels.map(({ id, name }) => (
-                        // <li>{name}</li>
-                    <Link key={`channel-${id}`} to={`/view-team/${team.id}/${id}`} className="channel">
+                    // <li>{name}</li>
+                    <Link
+                        key={`channel-${id}`}
+                        to={`/view-team/${team.id}/${id}`}
+                        className="channel"
+                    >
                         <li>{name}</li>
                     </Link>
                 ))}
@@ -40,9 +53,13 @@ const Channels = ({ team, username, channels, users, onAddChannelClick, onInvite
                     </li>
                 ))}
             </Menu.Item>
-            <Menu.Item>
-                <a href="#invite-people" onClick={onInvitePeopleClick}>+ Invite People</a>
-            </Menu.Item>
+            {isOwner && (
+                <Menu.Item>
+                    <a href="#invite-people" onClick={onInvitePeopleClick}>
+                        + Invite People
+                    </a>
+                </Menu.Item>
+            )}
         </Grid.Column>
     );
 };
